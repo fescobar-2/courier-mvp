4e2fb34ae268a88b350adf0d9bcc5b1994517b2e
fescobarzo <fescobarzo@bdp.com.py>
2025-05-26 13:11:13 -0300

    Setup Angular Structure folders

---
+++ frontend/src/app/packages/package-form/package-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-package-form',
+  imports: [],
+  templateUrl: './package-form.component.html',
+  styleUrl: './package-form.component.scss'
+})
+export class PackageFormComponent {
+
+}
